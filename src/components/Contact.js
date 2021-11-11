9117801c25802919796e0b1314096f8617d977e3
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-11-10 22:48:17 -0500

    added basic inputs to the contact page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from '../utils/helpers';
 
 export default function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+  //   // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } 
+  };
+
+  const handleFormSubmit = (e) => {
+  //   // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+  //   // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+  //     // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+
+  //   // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setEmail('');
+  };
+
   return (
     <div>
-      <h1>Contact Info</h1>
-      <br></br>
-      <ul className="contact-lis">
-          <li>
-          <a className="contact-links" href="mailto:Joseph.Eric.Fisher@Gmail.com">
-                    Joseph.Eric.Fisher@Gmail.com
-                </a>
-          </li>
-          <li>
-          <a className="contact-links" href="https://github.com/jfisher37" target="blank_" tabindex="0">
-                    Github
-                </a>
-          </li>
-          <li>
-          <a className="contact-links" href="https://www.linkedin.com/in/joseph-fisher-6217a063/" target="blank_" tabindex="0">
-                    Linkedin
-                </a>
-          </li>
-      </ul>
+    <h1>Contact Info</h1>
+    <br></br>
+    <div>
+      <p>Hello {userName}</p>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username"
+        />
+
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
     </div>
   );
 }
